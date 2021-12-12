83e35e51a50d8b0d90c77723178e981ab292bd66
Will Lemmons <will.e.lemmons@gmail.com>
2021-12-11 21:07:34 -0700

    Opps, forgot to commit

--- Phone Game 1/Assets/Scripts/GameManagerAlgorithm.cs
+++ Phone Game 1/Assets/Scripts/GameManagerAlgorithm.cs
@@ -10,32 +10,116 @@ public class GameManagerAlgorithm : MonoBehaviour
     public int colorSelect;
     private Renderer render;
     public float buttonOn;
+    public float waitTime;
+    public List<int> activePattern;
+
+    private float waitTimeCounter;
     private float buttonLightCounter;
+    private bool shouldBeLit;
+    private bool shouldDark;
+    private bool gameActive;
+    private int positionSequence;
+    private int inputPattern;
 
 
     private void Start()
     {
-        render = buttons[colorSelect].GetComponent<Renderer>();
+        render = buttons[activePattern[positionSequence]].GetComponent<Renderer>();
+        gameActive = false;
     }
 
     private void Update()
     {
-        if (buttonLightCounter > 0)
+        if (shouldBeLit)
         {
             buttonLightCounter -= Time.deltaTime;
+            
+            if(buttonLightCounter < 0)
+            { 
+                render.material.color = new Color(render.material.color.r, render.material.color.g, render.material.color.b, 1f);
+                shouldBeLit = false;
+                shouldDark = true;
+                waitTimeCounter = waitTime;
+                positionSequence++;
+            }
         }
-        else
+
+        if (shouldDark)
         {
-            render.material.color = new Color(render.material.color.r, render.material.color.g, render.material.color.b, 1f);
+            waitTimeCounter -= Time.deltaTime;
+            
+            if (positionSequence >= activePattern.Count)
+            {
+                shouldDark = false;
+                gameActive = true;
+            }
+            else
+            {
+                if (waitTimeCounter < 0)
+                {
+                    RenderColorSelect();
+                    
+                    buttonLightCounter = buttonOn;
+                    shouldBeLit = true;
+                    shouldDark = false;
+                }
+            }
         }
     }
 
     public void StartGame()
     {
+        activePattern.Clear();
+        positionSequence = 0;
+        inputPattern = 0;
+        
         colorSelect = Random.Range(0, buttons.Length);
         Debug.Log(colorSelect);
-        render.material.color = new Color(render.material.color.r, render.material.color.g, render.material.color.b, 0.5f);
+        activePattern.Add(colorSelect);
+        
+        RenderColorSelect();
 
         buttonLightCounter = buttonOn;
+        
+        shouldBeLit = true;
+    }
+
+    public void RenderColorSelect()
+    {
+        render = buttons[activePattern[positionSequence]].GetComponent<Renderer>();
+        render.material.color = new Color(render.material.color.r, render.material.color.g, render.material.color.b, 0.5f);
+    }
+
+    public void ColorPress(int whatButton)
+    {
+        if (gameActive)
+        {
+            if (activePattern[inputPattern] == whatButton)
+            {
+                Debug.Log("Correct");
+
+                inputPattern++;
+                if (inputPattern >= activePattern.Count)
+                {
+                    positionSequence = 0;
+                    inputPattern = 0;
+                    
+                    colorSelect = Random.Range(0, buttons.Length);
+                    activePattern.Add(colorSelect);
+        
+                    RenderColorSelect();
+
+                    buttonLightCounter = buttonOn;
+        
+                    shouldBeLit = true;
+                    gameActive = false;
+                }
+            }
+            else
+            {
+                Debug.Log("wrong");
+                gameActive = false;
+            }
+        }
     }
 }
